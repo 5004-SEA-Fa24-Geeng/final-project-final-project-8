57bf0c58dd87e8761ca703a216bdc59da9f88722
lucy-at-may4s <165765822+lucy-at-may4s@users.noreply.github.com>
2025-04-01 17:24:39 -0700

    update model and add Meal record

--- src/main/java/model/recipe/Recipe.java
+++ src/main/java/model/recipe/Recipe.java
@@ -3,25 +3,27 @@ package model.recipe;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-import java.util.List; /**
-     * Primary recipe to pass around between objects.
-     * Is immutable and uses Jackson annotations for serialization.
-     *
-     * @param recipeId the ID of the recipe
-     * @param recipeName the name of the recipe
-     * @param instructions the instructions of the recipe
-     * @param image the image of the recipe
-     * @param youtube the video link of the recipe
-     * @param category the category of the recipe
-     * @param area the area of the recipe
-     * @param ingredients the ingredients of the recipe
-     * @param measures the portion of the ingredient
-     * @return the recipe
-     */
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    @JsonPropertyOrder({"recipeId", "recipeName", "instructions", "image", "youtube",
-                        "category", "area", "ingredients", "measures"})
-    public record Recipe(int recipeId, String recipeName, String category, String area, String instructions,
-                         String youtube, String image, List<String> ingredients, List<String> measures) {
+import java.util.List;
 
-    }
+/**
+ * Primary recipe to pass around between objects.
+ * Is immutable and uses Jackson annotations for serialization.
+ *
+ * @param recipeId the ID of the recipe
+ * @param recipeName the name of the recipe
+ * @param instructions the instructions of the recipe
+ * @param image the image of the recipe
+ * @param youtube the video link of the recipe
+ * @param category the category of the recipe
+ * @param area the area of the recipe
+ * @param ingredients the ingredients of the recipe
+ * @param measures the portion of the ingredient
+ * @return the recipe
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonPropertyOrder({"recipeId", "recipeName", "instructions", "image", "youtube",
+                    "category", "area", "ingredients", "measures"})
+public record Recipe(int recipeId, String recipeName, String category, String area, String instructions,
+                     String youtube, String image, List<String> ingredients, List<String> measures) {
+
+}
