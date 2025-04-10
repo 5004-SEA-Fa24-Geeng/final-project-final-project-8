e1313f94642686a23418bd530d8a6cefc95cafa3
Lu-Yin Wang <145145367+rebwang@users.noreply.github.com>
2025-04-10 14:48:15 -0700

    update ApiUtils test (#33)

--- src/test/java/com/group8/foodwizard/model/api/ApiUtilsTest.java
+++ src/test/java/com/group8/foodwizard/model/api/ApiUtilsTest.java
@@ -1,10 +1,13 @@
 package com.group8.foodwizard.model.api;
 
 import com.group8.foodwizard.model.api.ApiUtils;
+import com.group8.foodwizard.model.recipe.Meal;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,10 +18,27 @@ public class ApiUtilsTest {
     public void testGetAllIngredients() {
         String json = "{\"meals\": []}";
         InputStream mockInputStream = new ByteArrayInputStream(json.getBytes());
-
         InputStream result = ApiUtils.getAllIngredients();
+        assertNotNull(result);
+        assertNotNull(mockInputStream);
+    }
 
-        assertNotNull(result, "The InputStream should not be null.");
+    @Test
+    public void testGetAllCategories() {
+        String json = "{\"meals\": []}";
+        InputStream mockInputStream = new ByteArrayInputStream(json.getBytes());
+        InputStream result = ApiUtils.getAllCategories();
+        assertNotNull(result);
+        assertNotNull(mockInputStream);
+    }
+
+    @Test
+    public void testGetAllAreas() {
+        String json = "{\"meals\": []}";
+        InputStream mockInputStream = new ByteArrayInputStream(json.getBytes());
+        InputStream result = ApiUtils.getAllAreas();
+        assertNotNull(result);
+        assertNotNull(mockInputStream);
     }
 
     @Test
@@ -57,5 +77,42 @@ public class ApiUtilsTest {
         InputStream result = ApiUtils.getRecipeByIdMeal(52940);
 
         assertNotNull(result, "The InputStream should not be null.");
+    }
+
+    @Test
+    public void testGetMealsSetByIngredient() throws IOException {
+        Set<Meal> expected = new HashSet<>();
+        expected.add(new Meal("Honey Balsamic Chicken with Crispy Broccoli & Potatoes",
+                "https://www.themealdb.com/images/media/meals/kvbotn1581012881.jpg","52993"));
+        String ingredientName = "Balsamic Vinegar";
+        try {
+            Set<Meal> result = ApiUtils.getMealsByIngredient(ingredientName);
+            assertEquals(expected, result);
+        }
+        catch (IOException e) {
+            throw new IOException("exception");
+        }
+    }
+
+    @Test
+    public void testMealsByCategory() {
+        String category = "Chicken";
+        InputStream result = ApiUtils.mealsByCategory(category);
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testMealsByArea() {
+        String area = "Canadian";
+        InputStream result = ApiUtils.mealsByCategory(area);
+        assertNotNull(result);
+    }
+
+    @Test
+    public void testBadUrlContents() throws IOException {
+        String badUrl = "ht!tp://bad-url";
+        InputStream result = ApiUtils.getUrlContents(badUrl);
+        assertNotNull(result);  // Should return InputStream.nullInputStream()
+        assertEquals(0, result.readAllBytes().length);
     }
 }
